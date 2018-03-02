f881448b1c4e19e9b5db87b270db760f137f413d
qiyei2015 <1273482124@qq.com>
2018-03-02 20:56:31 +0800

    添加最简单的表达式计算

---
+++ src/com/qiyei/util/LogUtil.java
@@ -0,0 +1,20 @@
+package com.qiyei.util;
+
+/**
+ * @author Created by qiyei2015 on 2018/1/6.
+ * @version: 1.0
+ * @email: 1273482124@qq.com
+ * @description:
+ */
+public class LogUtil {
+
+    /**
+     * 显示日志
+     * @param TAG
+     * @param message
+     */
+    public static void println(String TAG,String message){
+        System.out.println(TAG + " " + message);
+    }
+
+}
