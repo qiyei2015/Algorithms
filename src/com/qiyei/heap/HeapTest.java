5bb02e110dc9db3d100cd5e62de39192757c8c7b
qiyei2015 <1273482124@qq.com>
2018-04-20 20:26:10 +0800

    增加索引最小堆

--- src/com/qiyei/heap/HeapTest.java
+++ src/com/qiyei/heap/HeapTest.java
@@ -11,21 +11,14 @@ import java.util.Random;
  * @description:
  */
 public class HeapTest {
-
-
-
     static Integer[] array = new Integer[100];
     static Random random = new Random();
 
-
-
     public static void main(String[] args){
-        testHeap();
-        testIndexMaxHeap();
+//        testHeap();
+//        testIndexMaxHeap();
+        testIndexMinHeap();
     }
-
-
-
 
     /**
      * 测试对
@@ -55,7 +48,11 @@ public class HeapTest {
         //索引堆测试
         IndexMaxHeap<Integer> indexMaxHeap = new IndexMaxHeap<>(100);
         for (int i = 0 ; i < 100;i++){
-            indexMaxHeap.insert(i,random.nextInt(100));
+            int value = random.nextInt(100);
+            if ( i == 5){
+                LogUtil.println("indexMaxHeap index:" + i  + " "+ value);
+            }
+            indexMaxHeap.insert(i,value);
         }
         LogUtil.println("indexMaxHeap size:" + indexMaxHeap.size());
         LogUtil.println("indexMaxHeap getIndexMax():" + indexMaxHeap.getIndexMax());
@@ -65,6 +62,31 @@ public class HeapTest {
 
         for (int i = 0 ; i < 100;i++){
             LogUtil.println("[ " + indexMaxHeap.getIndexMax() + " " + indexMaxHeap.delMax() + " ]");
+        }
+    }
+
+
+    /**
+     * 测试最小索引堆
+     */
+    private static void testIndexMinHeap(){
+        //索引堆测试
+        IndexMinHeap<Integer> indexMinHeap = new IndexMinHeap<>(100);
+        for (int i = 0 ; i < 100;i++){
+            int value = random.nextInt(100);
+            if ( i == 5){
+                LogUtil.println("indexMinHeap index:" + i  + " "+ value);
+            }
+            indexMinHeap.insert(i,value);
+        }
+        LogUtil.println("indexMinHeap size:" + indexMinHeap.size());
+        LogUtil.println("indexMinHeap getIndexMin():" + indexMinHeap.getIndexMin());
+        LogUtil.println("indexMinHeap getMin:" + indexMinHeap.getMin());
+
+        LogUtil.println("indexMinHeap getItemIndex(5):" + indexMinHeap.getItemIndex(5));
+
+        for (int i = 0 ; i < 100;i++){
+            LogUtil.println("[ " + indexMinHeap.getIndexMin() + " " + indexMinHeap.delMin() + " ]");
         }
     }
 
