59f6f1f3a0e8d74a4bd2064d80f2f643ea04b036
qiyei2015 <1273482124@qq.com>
2018-06-24 11:22:34 +0800

    thoughtwork

--- src/com/qiyei/graph/th/Client.java
+++ src/com/qiyei/graph/th/Client.java
@@ -4,7 +4,6 @@ package com.qiyei.graph.th;
 import com.qiyei.util.LogUtil;
 
 import java.io.File;
-import java.util.TreeMap;
 
 /**
  * @author Created by qiyei2015 on 2018/6/23.
@@ -27,13 +26,10 @@ public class Client {
 
 
     public static void main(String[] args){
-        test();
+        testTrains();
     }
 
-    /**
-     * 测试Dijkstra算法
-     */
-    private static void test(){
+    private static void testTrains(){
         IWeightGraph<Town,Integer> townGraph = (TownWeightGraph) GraphReadUtil.readWeightGraph(new File(directGraph),true);
 
         //A:0 B:1 C:2 D:3 E:4
@@ -52,15 +48,12 @@ public class Client {
 
         SolutionTrains<Town,Integer> solutionTrains = new SolutionTrains<>(townGraph);
 
-        SolutionTrains<Town,Integer> shortestSolutionTrains1 = new SolutionTrains<>(townGraph,new Town(A).key());
+        Dijkstra dijkstra = new Dijkstra(townGraph,new Town(A).key());
 
         LogUtil.println("Output #6:" + solutionTrains.getTripsMaxStop(new Town(C),new Town(C),3).size());
         LogUtil.println("Output #7:" + solutionTrains.getTripsExactlyStop(new Town(A),new Town(C),4).size());
-
-        LogUtil.println("Output #8:" + shortestSolutionTrains1.shortestPathTo(new Town(C).key()).intValue());
-
-//        LogUtil.println("Output #9:" + shortestSolutionTrains2.shortestPathTo(new Town(B).key()));
-        test2(townGraph,new Town(B).key());
+        LogUtil.println("Output #8:" + dijkstra.shortestPathTo(new Town(C).key()));
+        LogUtil.println("Output #9:" + Dijkstra.getshortestPathAsSame(townGraph,new Town(B).key()));
         LogUtil.println("Output #10:" + solutionTrains.getDifferentRoutes(new Town(C),new Town(C),30).size());
 
     }
@@ -68,26 +61,6 @@ public class Client {
 
     private static String formatResult(Integer res){
         return res != null ? res + "" : NO_SUCH_ROUTE;
-    }
-
-    private static void test2(IWeightGraph<Town,Integer> graph,int source){
-
-        TreeMap<Integer,Town> record = new TreeMap<>();
-
-        //找到 w 到 v的最短路径
-        for (Edge<Town,Integer> edge : graph.adj(new Town(source))){
-            Town w = edge.other(new Town(source));
-
-            SolutionTrains<Town,Integer> solution = new SolutionTrains<>(graph,w.key());
-            Integer weight = solution.shortestPathTo(source).intValue() + edge.getWeight();
-            record.put(weight,w);
-        }
-
-        //取最小值
-        Town town = record.get(record.firstKey());
-        SolutionTrains<Town,Integer> solution = new SolutionTrains<>(graph,town.key());
-
-        LogUtil.println(" path:" + (solution.shortestPathTo(source).intValue() + graph.getWeight(new Town(source),town).intValue()));
     }
 
 }
--- src/com/qiyei/graph/th/SolutionTrains.java
+++ src/com/qiyei/graph/th/SolutionTrains.java
@@ -1,12 +1,9 @@
 package com.qiyei.graph.th;
 
 
-import com.qiyei.util.LogUtil;
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Stack;
 
 /**
  * @author Created by qiyei2015 on 2018/6/22.
@@ -25,134 +22,8 @@ public class SolutionTrains<T extends Number & Comparable<T>,W extends Number & 
      */
     private List<List<T>> res = new ArrayList<>();
 
-    /**
-     * 最短路径的起始点 源
-     */
-    private int source;
-    /**
-     * 最短路径上权值 distTo[i]存储从起始点s到i的最短路径长度
-     */
-    private Number[] distTo;
-    /**
-     * 标记顶点i是否被访问
-     */
-    private boolean[] marked;
-    /**
-     * from[i]记录最短路径中, 到达i点的边是哪一条,可以用来恢复整个最短路径
-     */
-    private Edge<T,W>[] from;
-
-
     public SolutionTrains(IWeightGraph mGraph) {
         this.mGraph = mGraph;
-    }
-
-
-    /**
-     * 构造函数
-     * @param graph
-     * @param s
-     */
-    public SolutionTrains(IWeightGraph<T,W> graph, int s) {
-        this.mGraph = graph;
-        this.source = s;
-        distTo = new Number[mGraph.V()];
-        marked = new boolean[mGraph.V()];
-        from = new Edge[mGraph.V()];
-
-        //最小索引堆，辅助数据结构
-        IndexMinHeap<W> indexMinHeap = new IndexMinHeap<>(mGraph.V());
-
-        //Dijkstra 算法
-        distTo[source] = 0;
-        marked[source] = true;
-        from[source] = new Edge<T,W>((T)new Town(source),(T)new Town(source),(W)(Number)0.0);
-        indexMinHeap.insert(source,(W)distTo[source]);
-
-        while (!indexMinHeap.isEmpty()){
-
-            //取出权值最小的那条边的结点
-            int v = indexMinHeap.extractIndexMin();
-            marked[v] = true;
-
-            //访问v的所有邻接边
-            for (Edge<T,W> edge : mGraph.adj((T)new Town(v))){
-                Town w = (Town) edge.other((T)new Town(v));
-                //w点没有被标记过
-                if (!marked[w.key()]){
-                    //没有被访问过，并且通过v来访问，距离更短，则更新
-                    if (from[w.key()] == null || distTo[v].doubleValue() + edge.getWeight().doubleValue() < distTo[w.key()].doubleValue()){
-                        distTo[w.key()] = distTo[v].doubleValue() + edge.getWeight().doubleValue();
-                        from[w.key()] = edge;
-                        if (indexMinHeap.contains(w.key())){
-                            indexMinHeap.replace(w.key(),edge.getWeight());
-                        }else {
-                            indexMinHeap.insert(w.key(),edge.getWeight());
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    /**
-     * 到w点的最短路径长度
-     * @param w
-     * @return
-     */
-    public Number shortestPathTo(int w){
-        return distTo[w];
-    }
-
-    /**
-     * 判断从source 到w点是否有最短路径
-     * @param w
-     * @return
-     */
-    public boolean hasPath(int w){
-        return marked[w];
-    }
-
-    /**
-     * 从source到w的最短路径
-     * @param w
-     * @return
-     */
-    public List<Edge<T,W>> shortestPath(int w){
-        List<Edge<T,W>> list = new ArrayList<>();
-
-        // 通过from数组逆向查找到从s到w的路径, 存放到栈中
-        Stack<Edge<T,W>> stack = new Stack<Edge<T,W>>();
-        Edge<T,W> edge = from[w];
-        Town v = (Town) edge.getV();
-        while(v.key() != source ){
-            stack.push(edge);
-            edge = from[((Town)edge.getV()).key()];
-        }
-        stack.push(edge);
-
-        // 从栈中依次取出元素, 获得顺序的从s到w的路径
-        while(!stack.empty() ){
-            edge = stack.pop();
-            list.add(edge);
-        }
-        return list;
-    }
-
-    /**
-     * 打印从source 到 w点的最短路径
-     * @param w
-     */
-    public void showShortestPath(int w){
-        LogUtil.println("shortest weight:" + distTo[w]);
-        Number result = new Double(0);
-        for (Edge edge : shortestPath(w)){
-            result = result.doubleValue() + edge.getWeight().doubleValue();
-            LogUtil.println(edge.toString() + " ");
-        }
-
-        LogUtil.println("result weight:" + result.doubleValue());
-
     }
 
     /**
