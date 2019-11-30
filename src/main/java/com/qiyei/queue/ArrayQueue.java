421b793d653cd27a2a178308a3b3658b2e2af7d2
qiyei2015 <1273482124@qq.com>
2019-11-30 21:38:38 +0800

    增加队列的数组实现

---
+++ src/main/java/com/qiyei/queue/ArrayQueue.java
@@ -0,0 +1,63 @@
+package com.qiyei.queue;
+
+import com.qiyei.array.Array;
+
+/**
+ * @author Created by qiyei2015 on 2019/11/30.
+ * @version: 1.0
+ * @email: 1273482124@qq.com
+ * @description:
+ */
+public class ArrayQueue<E> implements IQueue<E>{
+
+    private Array<E> array;
+
+
+    public ArrayQueue() {
+        this(10);
+    }
+
+    public ArrayQueue(int capacity){
+        array = new Array<>(capacity);
+    }
+
+    @Override
+    public void enqueue(E e) {
+        array.addLast(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return array.removeFirst();
+    }
+
+    @Override
+    public E getFront() {
+        return array.getFirst();
+    }
+
+    @Override
+    public int size() {
+        return array.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return array.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(String.format("Queue:size=%d\n",array.size()));
+        builder.append("[");
+        for (int i = 0 ; i < size() ; i++){
+            builder.append(array.get(i));
+            if (i != size() - 1){
+                builder.append(",");
+            }
+        }
+        builder.append("]");
+        return builder.toString();
+    }
+}
---
+++ src/test/java/com/qiyei/queue/ArrayQueueTest.java
@@ -0,0 +1,66 @@
+package com.qiyei.queue;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Created by qiyei2015 on 2019/11/30.
+ * @version: 1.0
+ * @email: 1273482124@qq.com
+ * @description:
+ */
+public class ArrayQueueTest {
+
+    ArrayQueue<Integer> queue;
+
+
+    @Before
+    public void setUp() throws Exception {
+        queue = new ArrayQueue<>(10);
+        for (int i = 0 ;i < 9;i++){
+            queue.enqueue(i);
+        }
+    }
+
+    @Test
+    public void enqueue() {
+        for (int i = 0 ;i < 9;i++){
+            queue.enqueue(i);
+        }
+        System.out.println(queue.toString());
+    }
+
+    @Test
+    public void dequeue() {
+        for (int i = 0 ;i < 5;i++){
+            System.out.println("dequeue=" + queue.dequeue());
+        }
+        System.out.println(queue.toString());
+    }
+
+    @Test
+    public void getFront() {
+        for (int i = 0 ;i < 5;i++){
+            System.out.println("getFront=" + queue.getFront());
+        }
+        System.out.println(queue.toString());
+    }
+
+    @Test
+    public void size() {
+        Assert.assertEquals(queue.size(),9);
+    }
+
+    @Test
+    public void isEmpty() {
+        Assert.assertEquals(queue.isEmpty(),false);
+    }
+
+    @Test
+    public void testToString() {
+
+    }
+}
