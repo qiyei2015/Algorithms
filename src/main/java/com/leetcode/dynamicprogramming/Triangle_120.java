7288d86a1025764656d97108a1470a9f6957062d
qiyei2015 <1273482124@qq.com>
2020-04-22 23:01:12 +0800

    leetcode120

--- src/main/java/com/leetcode/dynamicprogramming/Triangle_120.java
+++ src/main/java/com/leetcode/dynamicprogramming/Triangle_120.java
@@ -11,50 +11,36 @@ import java.util.List;
  */
 public class Triangle_120 {
 
-    private MinRes[] memo;
+    private int[][] memo;
 
-    private static class MinRes{
-        int sum;
-        int index;
-    }
 
     public int minimumTotal(List<List<Integer>> triangle) {
-        //size 行
-        memo = new MinRes[triangle.size()];
-        for (int i = 0 ;i < memo.length ;i++){
-            memo[i] = new MinRes();
+        memo = new int[triangle.size()][triangle.size()];
+
+        // dp[i,j] = min(Cij + dp[i][j+1],Cij + dp[i+1][j+1])
+        for (int i = triangle.get(triangle.size() - 1).size() - 1; i >= 0; i--) {
+            memo[triangle.size() - 1][i] = triangle.get(triangle.size() - 1).get(i);
         }
-
-        if (triangle.size() == 1){
-            memo[0] = min(triangle.get(0),0,0);
-            return memo[0].sum;
-        }
-
-        memo[0] = min(triangle.get(0),0,0);
-        MinRes r = min(triangle.get(1),Math.max(0,memo[0].index - 1),Math.min(memo[0].index + 1,triangle.get(1).size() - 1));
-        memo[1].sum = memo[0].sum + r.sum;
-        memo[1].index = r.index;
-
-        for (int i = 2 ;i < triangle.size() ;i++){
-            MinRes res = min(triangle.get(i),Math.max(0,memo[i-1].index - 1),Math.min(memo[i-1].index + 1,triangle.get(i).size() - 1));
-            memo[i].sum = memo[i -1].sum + res.sum;
-            memo[i].index = res.index;
-        }
-        return memo[triangle.size() - 1].sum;
-
-    }
-
-
-    private MinRes min(List<Integer> list,int start,int end){
-        MinRes res = new MinRes();
-        res.sum = list.get(start);
-        res.index = start;
-        for (int i = start ;i <= end ;i++){
-            if (res.sum > list.get(i)){
-                res.sum = list.get(i);
-                res.index = i;
+        for (int i = triangle.size() - 2; i >= 0; i--) {
+            for (int j = triangle.get(i).size() - 1; j >= 0; j--) {
+                memo[i][j] = Math.min(triangle.get(i).get(j) + memo[i + 1][j], triangle.get(i).get(j) + memo[i + 1][j + 1]);
             }
         }
-        return res;
+        return memo[0][0];
+    }
+
+    public int minimumTotal2(List<List<Integer>> triangle) {
+        int[][] memo2 = new int[2][triangle.get(triangle.size() - 1).size()];
+
+        // dp[i,j] = min(Cij + dp[i][j+1],Cij + dp[i+1][j+1])
+        for (int i = triangle.get(triangle.size() - 1).size() - 1; i >= 0; i--) {
+            memo2[(triangle.size() - 1) % 2][i] = triangle.get(triangle.size() - 1).get(i);
+        }
+        for (int i = triangle.size() - 2; i >= 0; i--) {
+            for (int j = triangle.get(i).size() - 1; j >= 0; j--) {
+                memo2[i % 2][j] = Math.min(triangle.get(i).get(j) + memo2[(i + 1) % 2][j], triangle.get(i).get(j) + memo2[(i + 1) % 2][j + 1]);
+            }
+        }
+        return memo2[0][0];
     }
 }
